929f6cfa3e13e5d257d235fdd1df29a74dafd707
Luis Dias <Luis.DB.Dias@ctw.bmwgroup.com>
2020-05-01 13:37:05 +0100

    Enhance and control turtle bot from twist

---
+++ src/application/Application.h
@@ -0,0 +1,41 @@
+/**
+ * \file
+ * \brief       Application node
+ *
+ * \project     TWIST
+ * \copyright   FIKALAB
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "business/flow/InitManager.h"
+
+/**
+ * @ingroup twist
+ *
+ * Namespace for all TWIST modules
+ */
+namespace twist {
+
+class Application final {
+public:
+    /**
+     * Constructor
+     *
+     * @param nodeHandle node handle from main
+     */
+    explicit Application(ros::NodeHandle& nodeHandle);
+
+    /**
+     * Destructor default
+     */
+    ~Application() = default;
+
+private:
+    std::unique_ptr<InitManager> initManager;
+};
+
+}  // namespace twist
+
